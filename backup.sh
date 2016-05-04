72d1d174b6b4871055cae1a0dbf30741ddbb907f
= <vlolwane@g.harvard.edu>
2016-05-04 10:04:45 +0200

    minnor changes

--- backup.sh
+++ backup.sh
@@ -1,7 +1,2 @@
-echo "launching the site"
-sudo a2dissite default
-sudo a2ensite ckan.lo
-sudo service apache2 restart
-
 echo "backup database"
-paster db dumb —config=/etc/ckan/default /production.ini ckan_database.pg_dumb 
+paster db dumb —config=/etc/ckan/default/production.ini ckan_database.pg_dumb 
