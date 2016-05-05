2b914b6b3fa4aaec23173ea42493554ebd1d1ef9
= <vlolwane@g.harvard.edu>
2016-05-05 09:44:30 +0200

    minor syntax changes

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 echo "backup database"
-paster db dumb —config=/etc/ckan/default/production.ini ckan_database.pg_dumb 
+paster db dump —config=/etc/ckan/default/production.ini ckan_database.pg_dump
--- load.sh
+++ load.sh
@@ -2,4 +2,4 @@ echo "cleaning database"
 paster db clean -c /etc/ckan/default/production.ini 
 
 echo "loading database"
-paster db load -c /etc/ckan/default/production.ini ckan_database.pg_dumb
+paster db load -c /etc/ckan/default/production.ini ckan_database.pg_dump
