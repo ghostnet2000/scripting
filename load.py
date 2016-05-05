cb14829b4511c78f13d711075f9f4fed1872ef46
= <vlolwane@g.harvard.edu>
2016-05-05 11:42:34 +0200

    changed production.ini to development.ini in load.py

--- load.py
+++ load.py
@@ -5,7 +5,7 @@ def main():
 	os.system('paster db clean -c /etc/ckan/default/development.ini')
 
 	#load
-	os.system('paster db load -c /etc/ckan/default/production.ini ckan_database.pg_dump')
+	os.system('paster db load -c /etc/ckan/default/development.ini ckan_database.pg_dump')
 
 if __name__ == __main__:
 	main()
