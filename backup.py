9861b70b43dd4f89ec0d10545c0cd8f1789f3eb7
= <vlolwane@g.harvard.edu>
2016-05-05 13:05:14 +0200

    minor changes

--- backup.py
+++ backup.py
@@ -1,7 +1,7 @@
 import os 
 
 def main():
-	os.system('paster db dump -—config=/etc/ckan/default/development.ini ckan_database.pg_dump')
+	os.system('paster db dump -c /etc/ckan/default/development.ini ckan_database.pg_dump')
 
 if __name__ == __main__:
 	main()
--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 echo 'backup database'
-paster db dump -—config=/etc/ckan/default/development.ini ckan_database.pg_dump
+paster db dump -c /etc/ckan/default/development.ini ckan_database.pg_dump
